a70ec73b8f449fcd76fab65dd98247c7d7e2ffb2
Priyansh Singh <singhpriyansh2000@gmail.com>
2022-03-10 03:42:57 +0530

    added draw_cards and shuffle_deck buttons

--- index.js
+++ index.js
@@ -1,24 +1,63 @@
 let deckId;
+const cardsContainer = document.getElementById("cards");
+const newDeckBtn = document.getElementById("new-deck");
+const drawCardsBtn = document.getElementById("draw-cards");
 
 function handleClick() {
   fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       deckId = data.deck_id;
     });
 }
 
-document.getElementById("new-deck").addEventListener("click", handleClick);
-document.getElementById("draw-cards").addEventListener("click", drawCards);
+newDeckBtn.addEventListener("click", handleClick);
+drawCardsBtn.addEventListener("click", drawCards);
 
 function drawCards() {
   fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
     .then((res) => res.json())
     .then((data) => {
-      document.getElementById("cards").innerHTML = `
-    <img src = ${data.cards[0].image} />
-    <img src = ${data.cards[1].image} />
-    `;
+      cardsContainer.children[0].innerHTML = `<img src = ${data.cards[0].image} class = "card"/>`;
+      cardsContainer.children[1].innerHTML = `<img src = ${data.cards[1].image} class = "card"/>`;
     });
 }
+
+function determineWinnerCard(card1, card2) {
+  let cardsArray = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "JACK",
+    "QUEEN",
+    "KING",
+    "ACE",
+  ];
+  const card1ValueIndex = cardsArray.indexOf(card1.value);
+  const card2ValueIndex = cardsArray.indexOf(card2.value);
+  console.log(card1ValueIndex);
+  console.log(card2ValueIndex);
+  if (card1ValueIndex > card2ValueIndex) {
+    console.log("Card1 is the Winner");
+  } else if (card2ValueIndex > card1ValueIndex) {
+    console.log("Card2 is the Winner");
+  } else {
+    console.log("Its a tie");
+  }
+}
+
+cardObj1 = {
+  value: "9",
+};
+
+cardObj2 = {
+  value: "ACE",
+};
+
+determineWinnerCard(cardObj1, cardObj2);
